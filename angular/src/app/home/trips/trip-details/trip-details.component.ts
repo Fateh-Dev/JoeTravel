9128db04513cb1a977a3e4d9a1ae2199e7d8723f
unknown <fateh_djehinet@outlook.com>
2023-02-03 15:26:00 -0800

    More details

--- angular/src/app/home/trips/trip-details/trip-details.component.ts
+++ angular/src/app/home/trips/trip-details/trip-details.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'; 
+import { ActivatedRoute } from '@angular/router';
 import { TripDto, TripService } from '@proxy';
+import { HomeService } from '../../home.service';
 
 @Component({
   selector: 'app-trip-details',
   templateUrl: './trip-details.component.html',
-  styleUrls: ['./trip-details.component.scss']
+  styleUrls: ['./trip-details.component.scss'],
 })
 export class TripDetailsComponent implements OnInit {
-  tripItem: TripDto
-  loading = false
-  constructor(public tripService: TripService, private route: ActivatedRoute) { }
+  tripItem: TripDto;
+  loading = false;
+  items: TripDto[] = [];
+  constructor(
+    public tripService: TripService,
+    private route: ActivatedRoute,
+    public homeService: HomeService
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
-      this.loading = true
-      this.tripService.get(paramMap.get('id')).subscribe(
-        e => {
-          this.tripItem = e
-          this.loading = false
-        }
-      )
-    })
+      this.loading = true;
+      this.tripService.get(paramMap.get('id')).subscribe(e => {
+        this.tripItem = e;
+        this.loading = false;
+      });
+    });
 
+    this.getTrips(this.homeService.searchForm.controls.title.value);
   }
-
+  getTrips(title?: string) {
+    this.loading = true;
+    console.log(title);
+    this.tripService
+      .getHomeList({ maxResultCount: 5, skipCount: 0, title: title })
+      .subscribe(e => {
+        this.items = e.items;
+        this.loading = false;
+        console.log(e);
+      });
+  }
 }
